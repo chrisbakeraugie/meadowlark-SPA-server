43fde459012a8e9eecf219221e38c9ec0b55ebf8
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-09-07 17:03:41 -0400

    Successfully connected to mongoDB databse

---
+++ db.js
@@ -0,0 +1,15 @@
+const {credentials} = require('./config');
+
+const mongoose = require('mongoose');
+const connectionString = credentials.mongo.connectionString;
+if(!connectionString) {
+  console.error("Connection string not reached.");
+  process.exit(1);
+}
+mongoose.connect(connectionString, { useNewUrlParser:true });
+const db = mongoose.connection;
+db.on('error', err => {
+  console.error('Mongo DB error: ' + err.message);
+  process.exit(1);
+});
+db.once('open', () => console.log("\nConnection established"));
