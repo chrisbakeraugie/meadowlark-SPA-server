e3dbf49eb46fae23d0596a0098ff1520d9ad1c8d
Chris Baker <chrisbaker@Chriss-MBP.home>
2020-09-08 23:47:53 -0400

    Completed db.js

--- db.js
+++ db.js
@@ -15,3 +15,21 @@ db.on('error', err => {
 });
 db.once('open', () => console.log("\nConnection established"));
 
+const Vacation = require('./models/VacationShema');
+const VacationInSeasonListener = require('./models/VacationInSeasonListenerSchema');
+const Attraction = require('./models/AttractionSchema');
+
+let options = {}
+module.exports = {
+  getVacations: async (options) => {Vacation.find(options)},
+  getVacationBySku: async (sku) => {Vacation.findOne({sku})},
+  addVacationInSeasonListener: async (email, sku) => {
+    await VacationInSeasonListener.updateOne(
+      { email },
+      { $push: {skus: sku}},
+      { upsert: true }
+    )
+  },
+  getAttractions: async (options) => {Attraction.find(options)},
+  addAttraction: async (addAttraction) => {new Attraction(addAttraction.save())}
+};
