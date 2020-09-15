82c2bf43342830a48aace541d14c7c2b82a0b5ab
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-09-15 18:28:08 -0400

    connected server.js to handlers

--- server.js
+++ server.js
@@ -2,3 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multiparty = require('multiparty');
 const cors = require('cors');
+
+const handlers = require('./lib/handlers');
+
+const { credentials } = require('./config');
+
+const db = require('./db');
+
+const app = express();
+
+app.use('/api', cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3033;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/vacations', handlers.getVacationApi);
+app.get('/api/vacations/:sku', handlers.getVacationBySkuAPI);
+app.post('/api/vacations/:sku/notify-when-in-season', handlers.addVacationInSeasonListenerAPI);
+app.delete('/api/vacation/:sku', handlers.requestDeleteVacationAPI);
+
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express server started on port:${port}`);
+  });
+} else {
+  module.exports = app;
+}
