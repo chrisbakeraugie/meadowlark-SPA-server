716c1988759f50f4d7489836cc07f69c66ebee61
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-09-16 21:42:36 -0400

    Fixed db.js syntax errors, put other files back

--- db.js
+++ db.js
@@ -19,17 +19,66 @@ const Vacation = require('./models/VacationShema');
 const VacationInSeasonListener = require('./models/VacationInSeasonListenerSchema');
 const Attraction = require('./models/AttractionSchema');
 
-let options = {}
+Vacation.find((err, vacations) => {
+  if(err) return console.error(err)
+  if(vacations.length > 2) return 
+
+  new Vacation({
+    name: 'Hood River Day Trip',
+    slug: 'hood-river-day-trip',
+    category: 'Day Trip',
+    sku: 'HR199',
+    description: 'Spend a day sailing on the Columbia and ' + 
+      'enjoying craft beers in Hood River!',
+    price: 99.95,
+    tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+    inSeason: true,
+    maximumGuests: 16,
+    available: true,
+    packagesSold: 0,
+  }).save()
+
+  new Vacation({
+    name: 'Oregon Coast Getaway',
+    slug: 'oregon-coast-getaway',
+    category: 'Weekend Getaway',
+    sku: 'OC39',
+    description: 'Enjoy the ocean air and quaint coastal towns!',
+    price: 269.95,
+    tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+    inSeason: false,
+    maximumGuests: 8,
+    available: true,
+    packagesSold: 0,
+  }).save()
+
+  new Vacation({
+      name: 'Rock Climbing in Bend',
+      slug: 'rock-climbing-in-bend',
+      category: 'Adventure',
+      sku: 'B99',
+      description: 'Experience the thrill of climbing in the high desert.',
+      price: 289.95,
+      tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing'],
+      inSeason: true,
+      requiresWaiver: true,
+      maximumGuests: 4,
+      available: false,
+      packagesSold: 0,
+      notes: 'The tour guide is currently recovering from a skiing accident.',
+  }).save()
+})
+
 module.exports = {
-  getVacations: async (options) => {Vacation.find(options)},
-  getVacationBySku: async (sku) => {Vacation.findOne({sku})},
+  getVacations: async (options = {}) => Vacation.find(options), // async enable asynchronous, promise based behavior
+  getVacationBySku: async sku => Vacation.findOne({ sku }),
   addVacationInSeasonListener: async (email, sku) => {
     await VacationInSeasonListener.updateOne(
       { email },
-      { $push: {skus: sku}},
+      { $push: { skus: sku } },
       { upsert: true }
     )
   },
-  getAttractions: async (options) => {Attraction.find(options)},
-  addAttraction: async (addAttraction) => {new Attraction(addAttraction.save())}
-};
+  getAttractions: async (options = {}) => Attraction.find(options),
+  addAttraction: async attraction => new Attraction(attraction).save(),
+}
